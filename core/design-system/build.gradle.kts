5741b4182c64d39ca485342239b242bc0bbf1976
BRBX <brbxmain@gmail.com>
2025-05-23 17:21:00 +0300

    start creating multimodule structure

---
+++ core/design-system/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    //Compose
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.design_system"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    //Compose
+    implementation(platform(libs.androidx.compose.bom))
+}

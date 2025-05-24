b7fa3f745f0db3dd363c36e0c7527d9c0a3ac228
BRBX <brbxmain@gmail.com>
2025-05-24 15:09:44 +0300

    connect theme to app module

--- app/src/main/java/com/example/soundrush/MainActivity.kt
+++ app/src/main/java/com/example/soundrush/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.soundrush.ui.theme.SoundRushTheme
+import com.example.design_system.theme.SoundRushTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SoundRushTheme {
-
+                NavGraph()
             }
         }
     }
--- app/src/main/java/com/example/soundrush/NavGraph.kt
+++ app/src/main/java/com/example/soundrush/NavGraph.kt
@@ -6,8 +6,8 @@ import androidx.navigation.compose.rememberNavController
 import com.example.composition_screen.navigation.compositionScreen
 import com.example.navbar_screens.settings_screen.navigation.settingsScreen
 import com.example.navbar_screens.user_favourites_screen.navigation.userFavouritesScreen
-import com.example.navbar_screens.user_playlists_screen.navigation.UserPlaylistsScreen
 import com.example.navbar_screens.user_playlists_screen.navigation.userPlaylistsScreen
+import com.example.onboarding_screen.navigation.OnBoardingScreenRoute
 import com.example.onboarding_screen.navigation.onBoardingScreen
 import com.example.playlist_screen.navigation.playlistScreen
 
@@ -17,7 +17,7 @@ fun NavGraph() {
 
     NavHost(
         navController = navController,
-        startDestination = UserPlaylistsScreen
+        startDestination = OnBoardingScreenRoute
     ) {
         onBoardingScreen()
 

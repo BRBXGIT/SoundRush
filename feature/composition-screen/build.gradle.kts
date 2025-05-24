21755f323df1abfc718b2e8366c0a0a935770319
BRBX <brbxmain@gmail.com>
2025-05-24 14:23:27 +0300

    create basic project multimodule structure

---
+++ feature/composition-screen/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    //Compose
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.example.composition_screen"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    //Compose
+    implementation(platform(libs.androidx.compose.bom))
+}

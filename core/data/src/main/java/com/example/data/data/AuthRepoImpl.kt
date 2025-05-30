95b2cb72b302c1cb9668c9483db9269eac2c3d37
BRBX <brbxmain@gmail.com>
2025-05-30 16:13:03 +0300

    savin access token works fine, now start creating web view for oauth

--- core/data/src/main/java/com/example/data/data/AuthRepoImpl.kt
+++ core/data/src/main/java/com/example/data/data/AuthRepoImpl.kt
@@ -4,21 +4,19 @@ import com.example.data.domain.AuthRepo
 import com.example.local.datastore.auth.AuthManager
 import com.example.local.datastore.auth.AuthState
 import jakarta.inject.Inject
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class AuthRepoImpl @Inject constructor(
     private val authManager: AuthManager
 ): AuthRepo {
 
-    override val accessTokenFlow: Flow<String?>
-        get() = authManager.accessTokenFlow
+    override val accessTokenFlow = authManager.accessTokenFlow
 
     override val authState = accessTokenFlow.map { token ->
         if (token.isNullOrBlank()) AuthState.LoggedOut else AuthState.LoggedIn
     }
 
     override suspend fun saveAccessToken(token: String) {
-        authManager.accessTokenFlow
+        authManager.saveAccessToken(token)
     }
 }

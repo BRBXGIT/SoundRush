b20dac32808652a2ba63b38331b570b88d6f5363
BRBX <brbxmain@gmail.com>
2025-09-06 18:54:43 +0300

    all vm tests created and passed

--- feature/home-screen/src/test/java/com/example/home_screen/HomeScreenVMTest.kt
+++ feature/home-screen/src/test/java/com/example/home_screen/HomeScreenVMTest.kt
@@ -1,14 +1,18 @@
 package com.example.home_screen
 
+import androidx.paging.AsyncPagingDataDiffer
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListUpdateCallback
 import app.cash.turbine.test
 import com.example.data.domain.HomeScreenRepo
 import com.example.design_system.snackbars.SnackbarController
 import com.example.home_screen.screen.HomeScreenIntent
 import com.example.home_screen.screen.HomeScreenState
 import com.example.home_screen.screen.HomeScreenVM
-import com.example.network.common.NetworkError
 import com.example.network.common.NetworkErrors
 import com.example.network.common.NetworkResponse
+import com.example.network.home_screen.models.user_playlists_response.Collection
 import io.mockk.coEvery
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
@@ -18,8 +22,9 @@ import junit.framework.TestCase.assertNull
 import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -103,18 +108,6 @@ class HomeScreenVMTest {
         }
     }
 
-    private fun <T> generateNetworkResponse(
-        response: T,
-        label: String,
-        error: NetworkError
-    ): NetworkResponse<T> {
-        return NetworkResponse(
-            response = response,
-            label = label,
-            error = error
-        )
-    }
-
     @Test
     fun `create playlist refresh playlists after success call`() = runTest {
         vm.homeScreenState.test {
@@ -125,7 +118,11 @@ class HomeScreenVMTest {
             val after = awaitItem()
             assertTrue(after.isCreatePlaylistBSVisible)
 
-            val successResponse = generateNetworkResponse(Unit, "Success", NetworkErrors.SUCCESS)
+            val successResponse = NetworkResponse(
+                response = Unit,
+                label = "Success",
+                error = NetworkErrors.SUCCESS
+            )
             coEvery { repo.createPlaylist(
                 any(),
                 any(),
@@ -144,7 +141,7 @@ class HomeScreenVMTest {
     @Test
     fun `create playlist calls snackbar method after error`() = runTest {
         SnackbarController.events.test {
-            val errorResponse = generateNetworkResponse(
+            val errorResponse = NetworkResponse(
                 response = Unit,
                 label = "Seems you are unauthorized",
                 error = NetworkErrors.UNAUTHORIZED
@@ -164,5 +161,112 @@ class HomeScreenVMTest {
 
             cancelAndIgnoreRemainingEvents()
         }
+    }
+
+    @Test
+    fun `delete playlists refresh playlists after success call and change delete mode on start`() = runTest {
+        vm.homeScreenState.test {
+            val initial = awaitItem()
+            assertEquals(0, initial.refreshTrigger)
+            assertFalse(initial.isInDeleteMode)
+
+            val urn = "123"
+            val name = "playlist"
+            vm.sendIntent(HomeScreenIntent.AddUrnToDeleteList(urn, name))
+
+            val successResult = NetworkResponse(
+                response = Unit,
+                error = NetworkErrors.SUCCESS,
+                label = "Success"
+            )
+            coEvery { repo.deletePlaylist(any(), any()) } returns successResult
+
+            vm.sendIntent(HomeScreenIntent.DeletePlaylists)
+            advanceUntilIdle()
+
+            skipItems(1)
+            val after = awaitItem()
+            assertFalse(after.isInDeleteMode)
+            assertEquals(1, after.refreshTrigger)
+        }
+    }
+
+    @Test
+    fun `delete playlists calls snackbar method after error`() = runTest {
+        SnackbarController.events.test {
+            val errorResponse = NetworkResponse(
+                response = Unit,
+                label = "Seems you are unauthorized",
+                error = NetworkErrors.UNAUTHORIZED
+            )
+
+            val urn = "123"
+            val name = "playlist"
+            vm.sendIntent(HomeScreenIntent.AddUrnToDeleteList(urn, name))
+
+            coEvery { repo.deletePlaylist(any(), urn) } returns errorResponse
+
+            vm.sendIntent(HomeScreenIntent.DeletePlaylists)
+            advanceUntilIdle()
+
+            val after = awaitItem()
+            assertEquals(after.message, "Problem with deleting playlist: $name")
+        }
+    }
+
+    @Test
+    fun `playlists emits data when accessToken and refreshTrigger change`() = runTest {
+        // Data mock
+        val collections = listOf(
+            Collection(id = 1, title = "First"),
+            Collection(id = 2, title = "Second")
+        )
+
+        // Flow mock
+        val pagingData = PagingData.from(collections)
+        val flow = flowOf(pagingData)
+
+        coEvery { repo.getPlaylists(any()) } returns flow
+
+        // Check playlists
+        vm.playlists.test {
+            vm.sendIntent(HomeScreenIntent.FetchAccessToken("token_123"))
+            advanceUntilIdle()
+
+            val item = awaitItem()
+
+            val differ = AsyncPagingDataDiffer(
+                diffCallback = diffCallbackStub(),
+                updateCallback = noopListCallback(),
+                mainDispatcher = Dispatchers.Main,
+                workerDispatcher = Dispatchers.Main,
+            )
+            differ.submitData(item)
+
+            advanceUntilIdle()
+
+            assertEquals(2, differ.itemCount)
+            assertEquals("First", differ.getItem(0)?.title)
+            assertEquals("Second", differ.getItem(1)?.title)
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    // Stub
+    private fun diffCallbackStub() = object : DiffUtil.ItemCallback<Collection>() {
+        override fun areItemsTheSame(oldItem: Collection, newItem: Collection) =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Collection, newItem: Collection) =
+            oldItem == newItem
+    }
+
+    // Stub
+    private fun noopListCallback() = object : ListUpdateCallback {
+        override fun onInserted(position: Int, count: Int) {}
+        override fun onRemoved(position: Int, count: Int) {}
+        override fun onMoved(fromPosition: Int, toPosition: Int) {}
+        override fun onChanged(position: Int, count: Int, payload: Any?) {}
     }
 }
